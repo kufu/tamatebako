b9e29aadae99d010389c5c216239ea0df0f78f11
AtsushiM <at.mizoue@gmail.com>
2022-01-14 17:00:46 +0900

    クロージャーを使って影響する変数を明確にする

--- packages/wareki/src/index.ts
+++ packages/wareki/src/index.ts
@@ -1,53 +1,58 @@
-const REIWA = '令和' as const
-const HEISEI = '平成' as const
-const SHOWA = '昭和' as const
-const TAISHO = '大正' as const
-const MEIJI = '明治' as const
-type Geongo = typeof REIWA | typeof HEISEI | typeof SHOWA | typeof TAISHO | typeof MEIJI
+const { WAREKI_START_YEARS, adDateStringReg, warekiReg, selectGengo } = (() => {
+  const REIWA = '令和' as const
+  const HEISEI = '平成' as const
+  const SHOWA = '昭和' as const
+  const TAISHO = '大正' as const
+  const MEIJI = '明治' as const
+  type Geongo = typeof REIWA | typeof HEISEI | typeof SHOWA | typeof TAISHO | typeof MEIJI
 
-const ERA_RANGE = [
-  [REIWA, 2019, 4, 30, HEISEI],
-  [HEISEI, 1989, 1, 7, SHOWA],
-  [SHOWA, 1926, 12, 24, TAISHO],
-  [TAISHO, 1912, 7, 29, MEIJI],
-] as const
-const WAREKI_START_YEARS = {
-  't': 1912,
-  'T': 1912,
-  [TAISHO]: 1912,
-  's': 1926,
-  'S': 1926,
-  [SHOWA]: 1926,
-  'h': 1989,
-  'H': 1989,
-  [HEISEI]: 1989,
-  'r': 2019,
-  'R': 2019,
-  [REIWA]: 2019,
-  'm': 1868,
-  'M': 1868,
-  [MEIJI]: 1868,
-} as const
-const SEPARATOR = '[:\\/\\-\\.\\s．年月日]'
-const adDateStringReg = new RegExp(`^([0-9]{4})(${SEPARATOR})?([0-9]{1,2})(${SEPARATOR})?([0-9]{1,2})([\\s．]([0-9]{2}):([0-9]{2})$)?`)
-const warekiReg = new RegExp(`^(${Object.keys(WAREKI_START_YEARS).join('|')})([0-9]{1,2})(${SEPARATOR})([0-9]{1,2})(${SEPARATOR})([0-9]{1,2})(${SEPARATOR}?)$`)
+  const WAREKI_BOUNDARY = [
+    [REIWA, 2019, 4, 30, HEISEI],
+    [HEISEI, 1989, 1, 7, SHOWA],
+    [SHOWA, 1926, 12, 24, TAISHO],
+    [TAISHO, 1912, 7, 29, MEIJI],
+  ] as const
+  const WAREKI_START_YEARS = {
+    't': 1912,
+    'T': 1912,
+    [TAISHO]: 1912,
+    's': 1926,
+    'S': 1926,
+    [SHOWA]: 1926,
+    'h': 1989,
+    'H': 1989,
+    [HEISEI]: 1989,
+    'r': 2019,
+    'R': 2019,
+    [REIWA]: 2019,
+    'm': 1868,
+    'M': 1868,
+    [MEIJI]: 1868,
+  } as const
 
-// TODO: ほかの全角文字も半角に治す必要があるかも？
-const fullWidthToHalfWidth = (dateString: string) => dateString.replace(/[ａ-ｚＡ-Ｚ０-９．]/g, ((s) => String.fromCharCode(s.charCodeAt(0) - 0xfee0)))
-const selectGengo = (year: number, month: number, date: number): Geongo => {
-  for (const [modern, boundaryYear, boundaryMonth, boundaryDay, ancient] of ERA_RANGE) {
-    if (year > boundaryYear) {
-      return modern
-    } else if (year === boundaryYear) {
-      if (month > boundaryMonth || (month === boundaryMonth && date > boundaryDay)) {
+  const SEPARATOR = '[:\\/\\-\\.\\s．年月日]'
+  const adDateStringReg = new RegExp(`^([0-9]{4})(${SEPARATOR})?([0-9]{1,2})(${SEPARATOR})?([0-9]{1,2})([\\s．]([0-9]{2}):([0-9]{2})$)?`)
+  const warekiReg = new RegExp(`^(${Object.keys(WAREKI_START_YEARS).join('|')})([0-9]{1,2})(${SEPARATOR})([0-9]{1,2})(${SEPARATOR})([0-9]{1,2})(${SEPARATOR}?)$`)
+
+  const selectGengo = (year: number, month: number, date: number): Geongo => {
+    for (const [modern, boundaryYear, boundaryMonth, boundaryDay, ancient] of WAREKI_BOUNDARY) {
+      if (year > boundaryYear) {
         return modern
+      } else if (year === boundaryYear) {
+        if (month > boundaryMonth || (month === boundaryMonth && date > boundaryDay)) {
+          return modern
+        }
+        return ancient
       }
-      return ancient
     }
+
+    return MEIJI
   }
 
-  return MEIJI
-}
+  return { WAREKI_START_YEARS, adDateStringReg, warekiReg, selectGengo }
+})()
+// TODO: ほかの全角文字も半角に治す必要があるかも？
+const fullWidthToHalfWidth = (dateString: string) => dateString.replace(/[ａ-ｚＡ-Ｚ０-９．]/g, ((s) => String.fromCharCode(s.charCodeAt(0) - 0xfee0)))
 
 type Result<T> = {
   isValid: boolean

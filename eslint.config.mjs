87cdafc68eb4ed20881f919eea9fbe0130b5c341
Tokky0425 <lily.yarn.5@gmail.com>
2024-12-17 11:46:30 +0900

    chore: 一旦 off になっているルールへの対応の意思を示すコメントを追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,14 +4,14 @@ export default [
   ...smarthr,
   {
     rules: {
-      'smarthr/require-barrel-import': 'off',
-      'smarthr/best-practice-for-date': 'off',
+      'smarthr/require-barrel-import': 'off', // TODO: 要対応
+      'smarthr/best-practice-for-date': 'off', // TODO: 要対応
     }
   },
   {
     files: ['packages/create-lint-set/**/*.ts'],
     rules: {
-      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-require-imports': 'off', // TODO: 要対応
     }
   },
   {

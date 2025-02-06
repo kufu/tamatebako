3538610dbd5e204b1d7257820269b98c7cb97e9c
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2025-02-06 14:14:15 +0900

    chore: 計算回数を最適化する

--- packages/eslint-plugin-smarthr/rules/a11y-input-has-name-attribute/index.js
+++ packages/eslint-plugin-smarthr/rules/a11y-input-has-name-attribute/index.js
@@ -1,47 +1,41 @@
-const JSON5 = require('json5');
-const fs = require('fs');
-const { generateTagFormatter } = require('../../libs/format_styled_components');
+const fs = require('fs')
+
+const JSON5 = require('json5')
+
+const { generateTagFormatter } = require('../../libs/format_styled_components')
 
 const OPTION = (() => {
-  const file = `${process.cwd()}/package.json`;
+  const file = `${process.cwd()}/package.json`
 
   if (!fs.existsSync(file)) {
-    return {};
+    return {}
   }
 
-  const json = JSON5.parse(fs.readFileSync(file));
-  const dependencies = [
-    ...Object.keys(json.dependencies || {}),
-    ...Object.keys(json.devDependencies || {}),
-  ];
+  const json = JSON5.parse(fs.readFileSync(file))
+  const dependencies = [...Object.keys(json.dependencies || {}), ...Object.keys(json.devDependencies || {})]
 
   return {
     react_hook_form: dependencies.includes('react-hook-form'),
-  };
-})();
+  }
+})()
 
 const EXPECTED_NAMES = {
   '(i|I)nput$': 'Input$',
   '(t|T)extarea$': 'Textarea$',
   '(s|S)elect$': 'Select$',
-  'InputFile$': 'InputFile$',
-  'RadioButton$': 'RadioButton$',
-  'RadioButtonPanel$': 'RadioButtonPanel$',
+  InputFile$: 'InputFile$',
+  RadioButton$: 'RadioButton$',
+  RadioButtonPanel$: 'RadioButtonPanel$',
   'Check(b|B)ox$': 'CheckBox$',
   'Combo(b|B)ox$': 'ComboBox$',
   '(Date|Wareki)Picker$': '(Date|Wareki)Picker$',
-  'TimePicker$': 'TimePicker$',
-  'DropZone$': 'DropZone$',
+  TimePicker$: 'TimePicker$',
+  DropZone$: 'DropZone$',
 }
 const TARGET_TAG_NAME_REGEX = new RegExp(`(${Object.keys(EXPECTED_NAMES).join('|')})`)
 const INPUT_NAME_REGEX = /^[a-zA-Z0-9_\[\]]+$/
 const INPUT_TAG_REGEX = /(i|I)nput$/
 const RADIO_BUTTON_REGEX = /RadioButton(Panel)?$/
-
-const findNameAttr = (a) => a?.name?.name === 'name'
-const findSpreadAttr = (a) => a.type === 'JSXSpreadAttribute'
-const findRadioInput = (a) => a.name?.name === 'type' && a.value.value === 'radio'
-const findReactHookFormRegisterSpreadAttr = (a) => a.type === 'JSXSpreadAttribute' && a.argument?.callee?.name === 'register'
 
 const MESSAGE_PART_FORMAT = `"${INPUT_NAME_REGEX.toString()}"にmatchするフォーマットで命名してください`
 const MESSAGE_UNDEFINED_NAME_PART = `
@@ -59,7 +53,7 @@ const SCHEMA = [
       checkType: { type: 'string', enum: ['always', 'allow-spread-attributes'], default: 'always' },
     },
     additionalProperties: false,
-  }
+  },
 ]
 
 /**
@@ -77,31 +71,49 @@ module.exports = {
     return {
       ...generateTagFormatter({ context, EXPECTED_NAMES }),
       JSXOpeningElement: (node) => {
-        const { name, attributes } = node;
-        const nodeName = name.name || '';
+        const { name, attributes } = node
+        const nodeName = name.name || ''
 
         if (nodeName.match(TARGET_TAG_NAME_REGEX)) {
-          const nameAttr = attributes.find(findNameAttr)
+          let nameAttr = null
+          let hasSpreadAttr = false
+          let hasReactHookFormRegisterSpreadAttr = false
+          let hasRadioInput = false
+
+          attributes.forEach((a) => {
+            if (a.type === 'JSXSpreadAttribute') {
+              hasSpreadAttr = true
+
+              if (hasReactHookFormRegisterSpreadAttr === false && a.argument?.callee?.name === 'register') {
+                hasReactHookFormRegisterSpreadAttr = true
+              }
+            } else {
+              switch (a.name?.name) {
+                case 'name': {
+                  nameAttr = a
+                  break
+                }
+                case 'type': {
+                  if (a.value.value === 'radio') {
+                    hasRadioInput = true
+                  }
+                  break
+                }
+              }
+            }
+          })
 
           if (!nameAttr) {
             if (
-              !(
-                OPTION.react_hook_form &&
-                attributes.some(findReactHookFormRegisterSpreadAttr)
-              ) &&
-              (attributes.length === 0 ||
-                checkType !== 'allow-spread-attributes' ||
-                !attributes.some(findSpreadAttr))
+              !(OPTION.react_hook_form && hasReactHookFormRegisterSpreadAttr) &&
+              (attributes.length === 0 || checkType !== 'allow-spread-attributes' || !hasSpreadAttr)
             ) {
-              const isRadio =
-                nodeName.match(RADIO_BUTTON_REGEX) ||
-                (nodeName.match(INPUT_TAG_REGEX) &&
-                  attributes.some(findRadioInput));
+              const isRadio = nodeName.match(RADIO_BUTTON_REGEX) || (nodeName.match(INPUT_TAG_REGEX) && hasRadioInput)
 
               context.report({
                 node,
                 message: `${nodeName} ${isRadio ? MESSAGE_UNDEFINED_FOR_RADIO : MESSAGE_UNDEFINED_FOR_NOT_RADIO}`,
-              });
+              })
             }
           } else {
             const nameValue = nameAttr.value?.value || ''
@@ -110,12 +122,12 @@ module.exports = {
               context.report({
                 node,
                 message: `${nodeName} のname属性の値(${nameValue})${MESSAGE_NAME_FORMAT_SUFFIX}`,
-              });
+              })
             }
           }
         }
       },
-    };
+    }
   },
-};
-module.exports.schema = SCHEMA;
+}
+module.exports.schema = SCHEMA

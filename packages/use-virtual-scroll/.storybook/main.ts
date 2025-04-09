d79365cf2a514eb3a9e1a534c2f32554a146ae62
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2025-04-09 12:40:50 +0900

    chore: 不要なパッケージ、設定をまるっと削除

--- packages/use-virtual-scroll/.storybook/main.ts
+++ packages/use-virtual-scroll/.storybook/main.ts
@@ -1,24 +1,11 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import type { StorybookConfig } from '@storybook/react-webpack5'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-webpack5-compiler-swc",
-    {
-      "name": "@storybook/addon-essentials",
-      "options": {
-        "docs": false
-      }
-    },
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
-  }
-};
-export default config;
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-webpack5-compiler-swc'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+}
+export default config

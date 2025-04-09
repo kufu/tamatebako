c676ef9978c35748387a740e214db9e09d6318f3
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2025-04-09 12:50:26 +0900

    chore: fix for eslint

--- packages/use-virtual-scroll/src/stories/useVirtualScroll.stories.tsx
+++ packages/use-virtual-scroll/src/stories/useVirtualScroll.stories.tsx
@@ -1,8 +1,8 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import { useState } from 'react'
 
 import { useVirtualScroll } from '../useVirtualScroll'
-import React from 'react'
-import { useState } from 'react'
+
+import type { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
   title: 'useVirtualScroll',
@@ -11,74 +11,78 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const ScrollOnWindow: Story = {
-  render: () => {
-    const originalItems = [...Array(1000)].map((_, i) => i)
-    const itemHeight = 20
-
-    const { items, listRef, createListStyle, createItemStyle } = useVirtualScroll<
-      (typeof originalItems)[number],
-      HTMLUListElement
-    >(originalItems, itemHeight)
-
-    return (
-      <ul ref={listRef} style={createListStyle()}>
-        {items.map((item, index) => (
-          <li key={index} style={{ height: itemHeight, ...createItemStyle(index) }}>
-            {item}
-          </li>
-        ))}
-      </ul>
-    )
-  },
+export const scrollOnWindow: Story = {
+  render: () => <ScrollOnWindow />,
 }
 
-export const ScrollOnContainer: Story = {
-  render: () => {
-    const originalItems = [...Array(1000)].map((_, i) => i)
-    const itemHeight = 20
-    const [containerHeight, setContainerHeight] = useState(300)
+export const scrollOnContainer: Story = {
+  render: () => <ScrollOnContainer />,
+}
 
-    const { items, listRef, scrollableContainerRef, createListStyle, createItemStyle } = useVirtualScroll<
-      (typeof originalItems)[number],
-      HTMLUListElement,
-      HTMLDivElement
-    >(originalItems, itemHeight)
+const ScrollOnWindow = () => {
+  const originalItems = [...Array(1000)].map((_, i) => i)
+  const itemHeight = 20
 
-    return (
-      <>
-        <div>
-          <label htmlFor="scroll-container-height">Scroll Container Height</label>
-        </div>
-        <input
-          name="height"
-          id="scroll-container-height"
-          type="number"
-          value={containerHeight}
-          onChange={(e) => setContainerHeight(Number(e.target.value))}
-        />
-        <div
-          ref={scrollableContainerRef}
-          style={{
-            position: 'fixed',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: 300,
-            height: containerHeight,
-            border: 'solid 1px #999',
-            overflow: 'auto',
-          }}
-        >
-          <ul ref={listRef} style={createListStyle()}>
-            {items.map((item, index) => (
-              <li key={index} style={{ height: itemHeight, ...createItemStyle(index) }}>
-                {item}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </>
-    )
-  },
+  const { items, listRef, createListStyle, createItemStyle } = useVirtualScroll<(typeof originalItems)[number], HTMLUListElement>(
+    originalItems,
+    itemHeight,
+  )
+
+  return (
+    <ul ref={listRef} style={createListStyle()}>
+      {items.map((item, index) => (
+        <li key={index} style={{ height: itemHeight, ...createItemStyle(index) }}>
+          {item}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+const ScrollOnContainer = () => {
+  const originalItems = [...Array(1000)].map((_, i) => i)
+  const itemHeight = 20
+  const [containerHeight, setContainerHeight] = useState(300)
+
+  const { items, listRef, scrollableContainerRef, createListStyle, createItemStyle } = useVirtualScroll<
+    (typeof originalItems)[number],
+    HTMLUListElement,
+    HTMLDivElement
+  >(originalItems, itemHeight)
+
+  return (
+    <>
+      <div>
+        <label htmlFor="scroll-container-height">Scroll Container Height</label>
+      </div>
+      <input
+        name="height"
+        id="scroll-container-height"
+        type="number"
+        value={containerHeight}
+        onChange={(e) => setContainerHeight(Number(e.target.value))}
+      />
+      <div
+        ref={scrollableContainerRef}
+        style={{
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: 300,
+          height: containerHeight,
+          border: 'solid 1px #999',
+          overflow: 'auto',
+        }}
+      >
+        <ul ref={listRef} style={createListStyle()}>
+          {items.map((item, index) => (
+            <li key={index} style={{ height: itemHeight, ...createItemStyle(index) }}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
 }

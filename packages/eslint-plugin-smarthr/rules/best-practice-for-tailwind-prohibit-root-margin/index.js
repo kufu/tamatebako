d20ba0f4629b4444afde7ca9b9737d12db570670
AtsushiM <at.mizoue@gmail.com>
2025-04-25 08:48:17 +0900

    chore: best-practice-for-tailwind-prohibit-root-marginの処理を最適化

--- packages/eslint-plugin-smarthr/rules/best-practice-for-tailwind-prohibit-root-margin/index.js
+++ packages/eslint-plugin-smarthr/rules/best-practice-for-tailwind-prohibit-root-margin/index.js
@@ -3,6 +3,8 @@ const { AST_NODE_TYPES } = require('@typescript-eslint/utils')
 const SCHEMA = []
 const MARGIN_CLASS_PATTERNS = /shr-m[trbl]?-/ // mt-, mr-, mb-, ml-, m-
 
+const findClassNameAttr = (attr) => attr.type === AST_NODE_TYPES.JSXAttribute && attr.name.name === 'className'
+
 /**
  * コンポーネントのルート要素を渡し、該当の余白クラスが存在すればそれを、なければNULLを返す
  * @param {import('@typescript-eslint/utils').TSESTree.Node} node
@@ -12,17 +14,18 @@ const findSpacingClassInRootElement = (node) => {
   // JSX でなければ対象外
   if (node.type !== AST_NODE_TYPES.JSXElement) return null
 
-  // className属性がなければ対象外
-  const classNameAttr = node.openingElement.attributes.find(
-    (attr) => attr.type === AST_NODE_TYPES.JSXAttribute && attr.name.name === 'className',
-  )
-  if (!classNameAttr) return null
+  const classNameAttr = node.openingElement.attributes.find(findClassNameAttr)
 
-  // className属性の値がリテラルでなければ対象外
-  if (classNameAttr?.value?.type !== AST_NODE_TYPES.Literal || typeof classNameAttr.value.value !== 'string') return null
+  if (
+    classNameAttr &&
+    // className属性の値がリテラル、かつ余白クラスの場合
+    classNameAttr.value?.type === AST_NODE_TYPES.Literal && typeof classNameAttr.value.value === 'string' &&
+    MARGIN_CLASS_PATTERNS.test(classNameAttr.value.value)
+  ) {
+    return classNameAttr.value
+  }
 
-  // className属性の値に余白クラスが含まれていればそれを返す
-  return MARGIN_CLASS_PATTERNS.test(classNameAttr.value.value) ? classNameAttr.value : null
+  return null
 }
 
 /**
@@ -56,30 +59,37 @@ module.exports = {
      * 関数本体をチェックし、ルート要素で余白クラスが設定されたJSXを返している場合、エラーを報告する
      * @param {import('@typescript-eslint/utils').TSESTree.Node} body
      */
-    const checkFunctionBody = (body) => {
-      // 関数がブロックを持たずに直接JSXを返すパターン
-      if (body.type === AST_NODE_TYPES.JSXElement) {
-        const spacingClass = findSpacingClassInRootElement(body)
-        if (spacingClass) {
-          context.report({
-            node: spacingClass,
-            messageId: 'noRootSpacing',
-          })
-        }
-        return
-      }
+    const checkFunctionBody = (n) => {
+      const body = n.body
 
-      // 関数がブロック内で JSX を return するパターン
-      if (body.type === AST_NODE_TYPES.BlockStatement) {
-        const returnStatement = findJSXReturnStatement(body)
-        if (returnStatement?.argument) {
-          const spacingClass = findSpacingClassInRootElement(returnStatement.argument)
+      switch (body.type) {
+        // 関数がブロックを持たずに直接JSXを返すパターン
+        case AST_NODE_TYPES.JSXElement: {
+          const spacingClass = findSpacingClassInRootElement(body)
+
           if (spacingClass) {
             context.report({
               node: spacingClass,
               messageId: 'noRootSpacing',
             })
           }
+
+          break
+        }
+        // 関数がブロック内で JSX を return するパターン
+        case AST_NODE_TYPES.BlockStatement: {
+          const returnStatement = findJSXReturnStatement(body)
+          if (returnStatement?.argument) {
+            const spacingClass = findSpacingClassInRootElement(returnStatement.argument)
+            if (spacingClass) {
+              context.report({
+                node: spacingClass,
+                messageId: 'noRootSpacing',
+              })
+            }
+          }
+
+          break
         }
       }
     }
@@ -88,14 +98,12 @@ module.exports = {
       // アロー関数式のコンポーネントをチェック
       ArrowFunctionExpression: (node) => {
         if (node.parent?.type === AST_NODE_TYPES.VariableDeclarator) {
-          checkFunctionBody(node.body)
+          checkFunctionBody(node)
         }
       },
 
       // function宣言のコンポーネントをチェック
-      FunctionDeclaration: (node) => {
-        checkFunctionBody(node.body)
-      },
+      FunctionDeclaration: checkFunctionBody,
     }
   },
 }

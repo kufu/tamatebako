f282499e71dca1312f5176682e02af0d7dc38273
AtsushiM <at.mizoue@gmail.com>
2025-05-08 08:52:31 +0900

    chore: a11y-trigger-has-buttonからコンポーネント名チェックを切り出す

--- packages/eslint-plugin-smarthr/test/component-name.js
+++ packages/eslint-plugin-smarthr/test/component-name.js
@@ -115,6 +115,21 @@ ruleTester.run('component-name', rule, {
     { code: `import { HogeSearchInput as FugaSearchInput } from './hoge'` },
 
     { code: 'const IndexNav = styled(HogeIndexNav)``' },
+
+    { code: `import { DropdownTrigger as HogeDropdownTrigger } from './hoge'` },
+    { code: `import { FugaDialogTrigger as HogeDialogTrigger } from './hoge'` },
+    { code: `import { AbcButton as HogeAbcButton } from './hoge'` },
+    { code: `import { AnchorButton as FugaAnchorButton } from './hoge'` },
+    { code: `import { HogeAnchor as HogeFugaAnchor } from './hoge'` },
+    { code: `import { FugaLink as HogeLink } from './hoge'` },
+    { code: 'const HogeAnchor = styled.a``' },
+    { code: 'const HogeLink = styled.a``' },
+    { code: 'const HogeButtonAnchor = styled(ButtonAnchor)``' },
+    { code: 'const HogeAnchorButton = styled(AnchorButton)``' },
+    { code: 'const HogeLink = styled(FugaLink)``' },
+    { code: 'const HogeAnchor = styled(FugaAnchor)``' },
+    { code: 'const HogeDialogTrigger = styled(DialogTrigger)``' },
+    { code: 'const HogeDropdownTrigger = styled(DropdownTrigger)``' },
   ],
   invalid: [
     { code: `import hoge from 'styled-components'`, errors: [ { message: `styled-components をimportする際は、名称が"styled" となるようにしてください。例: "import styled from 'styled-components'"` } ] },
@@ -267,5 +282,27 @@ ruleTester.run('component-name', rule, {
    - 修正例1: const Xxxx = styled(Hoge)
    - 修正例2: const SideNavXxxx = styled(Hoge)
    - 修正例3: const SideNav = styled(XxxxSideNav)` } ] },
+
+    { code: `import { DropdownTrigger as Hoge } from './hoge'`, errors: [ { message: `Hogeを正規表現 "/DropdownTrigger$/" がmatchする名称に変更してください。
+ - DropdownTriggerが型の場合、'import type { DropdownTrigger as Hoge }' もしくは 'import { type DropdownTrigger as Hoge }' のように明示的に型であることを宣言してください。名称変更が不要になります` } ] },
+    { code: `import { DialogTrigger as Hoge } from './hoge'`, errors: [ { message: `Hogeを正規表現 "/DialogTrigger$/" がmatchする名称に変更してください。
+ - DialogTriggerが型の場合、'import type { DialogTrigger as Hoge }' もしくは 'import { type DialogTrigger as Hoge }' のように明示的に型であることを宣言してください。名称変更が不要になります` } ] },
+    { code: `import { Button as Hoge } from './hoge'`, errors: [ { message: `Hogeを正規表現 "/Button$/" がmatchする名称に変更してください。
+ - Buttonが型の場合、'import type { Button as Hoge }' もしくは 'import { type Button as Hoge }' のように明示的に型であることを宣言してください。名称変更が不要になります` } ] },
+    { code: `import { AbcAnchorButton as Hoge } from './hoge'`, errors: [ { message: `Hogeを正規表現 "/Button$/" がmatchする名称に変更してください。
+ - AbcAnchorButtonが型の場合、'import type { AbcAnchorButton as Hoge }' もしくは 'import { type AbcAnchorButton as Hoge }' のように明示的に型であることを宣言してください。名称変更が不要になります` }, { message: `Hogeを正規表現 "/AnchorButton$/" がmatchする名称に変更してください。
+ - AbcAnchorButtonが型の場合、'import type { AbcAnchorButton as Hoge }' もしくは 'import { type AbcAnchorButton as Hoge }' のように明示的に型であることを宣言してください。名称変更が不要になります` } ] },
+    { code: `import { Anchor as Hoge } from './hoge'`, errors: [ { message: `Hogeを正規表現 "/Anchor$/" がmatchする名称に変更してください。
+ - Anchorが型の場合、'import type { Anchor as Hoge }' もしくは 'import { type Anchor as Hoge }' のように明示的に型であることを宣言してください。名称変更が不要になります` } ] },
+    { code: `import { Link as Hoge } from './hoge'`, errors: [ { message: `Hogeを正規表現 "/Link$/" がmatchする名称に変更してください。
+ - Linkが型の場合、'import type { Link as Hoge }' もしくは 'import { type Link as Hoge }' のように明示的に型であることを宣言してください。名称変更が不要になります` } ] },
+    { code: 'const Hoge = styled.a``', errors: [ { message: `Hogeを正規表現 "/(Anchor|Link)$/" がmatchする名称に変更してください。` } ] },
+    { code: 'const Hoge = styled(Button)``', errors: [ { message: `Hogeを正規表現 "/Button$/" がmatchする名称に変更してください。` } ] },
+    { code: 'const Hoge = styled(AnchorButton)``', errors: [ { message: `Hogeを正規表現 "/Button$/" がmatchする名称に変更してください。` }, { message: `Hogeを正規表現 "/AnchorButton$/" がmatchする名称に変更してください。` } ] },
+    { code: 'const Hoge = styled(ButtonAnchor)``', errors: [ { message: `Hogeを正規表現 "/Anchor$/" がmatchする名称に変更してください。` } ] },
+    { code: 'const Hoge = styled(Anchor)``', errors: [ { message: `Hogeを正規表現 "/Anchor$/" がmatchする名称に変更してください。` } ] },
+    { code: 'const Hoge = styled(Link)``', errors: [ { message: `Hogeを正規表現 "/Link$/" がmatchする名称に変更してください。` } ] },
+    { code: 'const Hoge = styled(DropdownTrigger)``', errors: [ { message: `Hogeを正規表現 "/DropdownTrigger$/" がmatchする名称に変更してください。` } ] },
+    { code: 'const Hoge = styled(DialogTrigger)``', errors: [ { message: `Hogeを正規表現 "/DialogTrigger$/" がmatchする名称に変更してください。` } ] },
   ]
 })

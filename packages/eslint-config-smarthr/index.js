4a1a279c9505daae8db724cba4de89fe95db679c
Mizoue Atsushi <asionfb@gmail.com>
2025-06-04 07:10:28 +0900

    feat: a11y-help-link-with-support-href, best-practice-for-nested-attributes-array-indexを有効化 (#659)

--- packages/eslint-config-smarthr/index.js
+++ packages/eslint-config-smarthr/index.js
@@ -36,15 +36,16 @@ export default [
       'smarthr/a11y-anchor-has-href-attribute': 'error',
       'smarthr/a11y-clickable-element-has-text': 'error',
       'smarthr/a11y-delegate-element-has-role-presentation': 'error',
-      'smarthr/a11y-form-control-in-form': 'warn', // TODO: 2024/11/1にwarn化。1mくらいを目処に問題なければerrorに変更予定
+      'smarthr/a11y-form-control-in-form': 'warn', // TODO: 2024/11にwarn化。問題なければerrorに変更予定
       'smarthr/a11y-heading-in-sectioning-content': 'error',
+      'smarthr/a11y-help-link-with-support-href': 'warn', // TODO: 2024/06にwarn化。問題なければerrorに変更予定
       'smarthr/a11y-image-has-alt-attribute': 'error',
       'smarthr/a11y-input-has-name-attribute': 'error',
       'smarthr/a11y-input-in-form-control': 'error',
       'smarthr/a11y-numbered-text-within-ol': 'error',
       'smarthr/a11y-prohibit-input-maxlength-attribute': 'error',
       'smarthr/a11y-prohibit-input-placeholder': 'error',
-      'smarthr/a11y-prohibit-sectioning-content-in-form': 'warn', // TODO: 2024/11/5にwarn化。1mくらいを目処に問題なければerrorに変更予定
+      'smarthr/a11y-prohibit-sectioning-content-in-form': 'warn', // TODO: 2024/11にwarn化。問題なければerrorに変更予定
       'smarthr/a11y-prohibit-useless-sectioning-fragment': 'error',
       'smarthr/a11y-replace-unreadable-symbol': 'error',
       'smarthr/a11y-trigger-has-button': 'error',
@@ -52,6 +53,7 @@ export default [
       'smarthr/best-practice-for-button-element': 'error',
       'smarthr/best-practice-for-date': 'error',
       'smarthr/best-practice-for-layouts': 'error',
+      'smarthr/best-practice-for-nested-attributes-array-index': 'warn', // TODO: 2025/06にwarn化。問題なければerrorに変更予定
       'smarthr/best-practice-for-remote-trigger-dialog': 'error',
       'smarthr/format-import-path': 'off',
       'smarthr/format-translate-component': 'off',

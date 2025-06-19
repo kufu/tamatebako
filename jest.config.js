fab5a9ec80d61e9419bc875162fda9d8e11baa7e
ushiboy <chihiro.ushiki@smarthr.co.jp>
2025-06-19 10:54:27 +0900

    chore: esmのみで公開されているnpmを使うためにjestの設定を調整

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts-esm',
   testRegex: '.*\\.test\\.tsx?$',
+  transformIgnorePatterns: ['node_modules/(?!(?:.pnpm/)?(use-intl|next-intl))'],
 }

3b7df81addfa8e558a76efac530e2894682be8f8
ushiboy <chihiro.ushiki@smarthr.co.jp>
2025-06-30 18:39:34 +0900

    fix: ディレクトリ移動して整理

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -1,3 +1,3 @@
 export { getLocale } from './get-locale/getLocale'
 export { useIntl } from './use-intl/useIntl'
-export { useNextIntl } from './useNextIntl'
+export { useNextIntl } from './use-intl/useNextIntl'

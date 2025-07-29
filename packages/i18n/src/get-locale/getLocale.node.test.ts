e5b72332de180f96adac5b06368353058aa6df7c
ushiboy <chihiro.ushiki@smarthr.co.jp>
2025-07-29 20:03:25 +0900

    feat: getLocaleの一部の判定をサーバサイドでも利用可能にした

--- packages/i18n/src/get-locale/getLocale.node.test.ts
+++ packages/i18n/src/get-locale/getLocale.node.test.ts
@@ -1,15 +1,51 @@
 /**
  * @jest-environment node
  */
-import { getLocale } from './getLocale'
+import { type Locale, getLocale } from './getLocale'
 
 describe('getLocale (node環境)', () => {
-  it('ブラウザ環境ではない場合、デフォルトの言語コードを返す', () => {
-    const result = getLocale({
-      currentLocale: 'en-US',
-      supportedLocales: ['en-US', 'ja-JP'],
-      shouldReturnDefaultLanguage: false,
+  // 共通のlocales配列を使用することを提案
+  const supportedLocales: Locale[] = ['en-US', 'ja-JP', 'ko-KR']
+
+  describe('shouldReturnDefaultLanguage が true の場合', () => {
+    it('デフォルトの言語コード（ja-JP）を返すこと', () => {
+      expect(
+        getLocale({
+          currentLocale: 'en-US',
+          supportedLocales,
+          shouldReturnDefaultLanguage: true,
+        }),
+      ).toBe('ja-JP')
     })
-    expect(result).toBe('ja-JP')
+  })
+  describe('locale が null の場合', () => {
+    it('デフォルトの言語コード（ja-JP）を返すこと', () => {
+      expect(
+        getLocale({
+          currentLocale: null,
+          supportedLocales,
+        }),
+      ).toBe('ja-JP')
+    })
+  })
+  describe('locale で指定された言語コードにアプリが対応している場合', () => {
+    it('指定された言語コードを返すこと', () => {
+      expect(
+        getLocale({
+          currentLocale: 'en-US',
+          supportedLocales,
+        }),
+      ).toBe('en-US')
+    })
+  })
+  describe('ブラウザ環境ではなく locale で指定された言語コードにアプリが対応していない場合', () => {
+    it('デフォルトの言語コード（ja-JP）を返すこと', () => {
+      const result = getLocale({
+        currentLocale: 'vi-VN',
+        supportedLocales,
+        shouldReturnDefaultLanguage: false,
+      })
+      expect(result).toBe('ja-JP')
+    })
   })
 })

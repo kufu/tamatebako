859ecc7e6451685947793b9ee9752f4993c5e033
Ryo Igarashi <n33t5hin@gmail.com>
2025-09-12 12:18:36 +0900

    chore: セルとチェックボックスの間にJSXFragmentが入っている場合のテストケースを追加

--- packages/eslint-plugin-smarthr/test/a11y-prohibit-checkbox-or-radio-in-table-cell.js
+++ packages/eslint-plugin-smarthr/test/a11y-prohibit-checkbox-or-radio-in-table-cell.js
@@ -21,8 +21,8 @@ ruleTester.run('a11y-prohibit-checkbox-or-radio-in-table-cell', rule, {
     },
     {
       code: `<Th><Checkbox /></Th>`,
-      errors: [{ message: 'Th の子孫に Checkbox を置くことはできません。代わりに ThCheckbox を使用してください。' }],
       output: `<ThCheckbox />`,
+      errors: [{ message: 'Th の子孫に Checkbox を置くことはできません。代わりに ThCheckbox を使用してください。' }],
     },
     {
       code: `<Th><Checkbox id="my-checkbox" name="agree" error /></Th>`,
@@ -33,8 +33,13 @@ ruleTester.run('a11y-prohibit-checkbox-or-radio-in-table-cell', rule, {
       code: `<Td><RadioButton /></Td>`,
       errors: [{ message: 'Td の子孫に RadioButton を置くことはできません。代わりに TdRadioButton を使用してください。' }],
     },
+
     {
       code: `<Td><div><div><Checkbox /></div></div></Td>`,
+      errors: [{ message: 'Td の子孫に Checkbox を置くことはできません。代わりに TdCheckbox を使用してください。' }],
+    },
+    {
+      code: `<Td><><><Checkbox /></></></Td>`,
       errors: [{ message: 'Td の子孫に Checkbox を置くことはできません。代わりに TdCheckbox を使用してください。' }],
     },
 

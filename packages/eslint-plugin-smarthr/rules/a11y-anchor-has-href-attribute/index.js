45de8437541e96e5f224024cf8002f10b82053a0
Mizoue Atsushi <asionfb@gmail.com>
2025-10-09 10:21:27 +0900

    chore: a11y-anchor-has-href-attributeの内部ロジックをselector形式に変更 (#820)

--- packages/eslint-plugin-smarthr/rules/a11y-anchor-has-href-attribute/index.js
+++ packages/eslint-plugin-smarthr/rules/a11y-anchor-has-href-attribute/index.js
@@ -4,90 +4,52 @@ const fs = require('fs')
 const OPTION = (() => {
   const file = `${process.cwd()}/package.json`
 
-  if (!fs.existsSync(file)) {
-    return {}
-  }
+  if (fs.existsSync(file)) {
+    const json = JSON5.parse(fs.readFileSync(file))
 
-  const json = JSON5.parse(fs.readFileSync(file))
-  const dependencies = [
-    ...(json.dependencies ? Object.keys(json.dependencies) : []),
-    ...(json.devDependencies ? Object.keys(json.devDependencies) : []),
-  ]
+    if (json.dependencies){
+      const dependencies = Object.keys(json.dependencies)
 
-  let nextjs = false
-  let react_router = false
-  const result = () => ({
-    nextjs,
-    react_router,
-  })
+      let nextjs = false
+      let react_router = false
+      const result = () => ({
+        nextjs,
+        react_router,
+      })
 
-  for (let i = 0; i < dependencies.length; i++) {
-    switch (dependencies[i]) {
-      case 'next':
-        nextjs = true
+      for (let i = 0; i < dependencies.length; i++) {
+        switch (dependencies[i]) {
+          case 'next':
+            nextjs = true
 
-        if (react_router) {
-          return result()
+            if (react_router) {
+              return result()
+            }
+
+            break
+          case 'react-router':
+            react_router = true
+
+            if (nextjs) {
+              return result()
+            }
+
+            break
         }
-
-        break
-      case 'react-router':
-        react_router = true
-
-        if (nextjs) {
-          return result()
-        }
-
-        break
+      }
     }
   }
 
-  return result()
+  return {}
 })()
 
-const REGEX_TARGET = /(Anchor|Link|^a)$/
-const check = (node, checkType) => {
-  const result = baseCheck(node, checkType)
+const ANCHOR_ELEMENT = 'JSXOpeningElement[name.name=/(Anchor|Link|^a)$/]'
+const HREF_ATTRIBUTE = `JSXAttribute[name.name=${OPTION.react_router ? '/^(href|to)$/' : '"href"'}]`
+const NEXT_LINK_REGEX = /Link$/
+// HINT: next/link で `Link>a` という構造がありえるので直上のJSXElementを調べる
+const nextCheck = (node) => ((node.parent.parent.openingElement.name.name || '').test(NEXT_LINK_REGEX))
 
-  return result && ((OPTION.nextjs && !nextCheck(node, checkType)) || (OPTION.react_router && !reactRouterCheck(node))) ? null : result
-}
-const baseCheck = (node, checkType) => {
-  const nodeName = node.name.name || ''
-
-  if (
-    REGEX_TARGET.test(nodeName) &&
-    checkExistAttribute(node, findHrefAttribute) &&
-    (checkType !== 'allow-spread-attributes' || !node.attributes.some(findSpreadAttr))
-  ) {
-    return nodeName
-  }
-
-  return false
-}
-const nextCheck = (node, checkType) => {
-  // HINT: next/link で `Link>a` という構造がありえるので直上のJSXElementを調べる
-  const target = node.parent.parent.openingElement
-
-  return target ? baseCheck(target, checkType) : false
-}
-const reactRouterCheck = (node) => checkExistAttribute(node, findToAttribute)
-
-const checkExistAttribute = (node, find) => {
-  const attr = node.attributes.find(find)?.value
-
-  return (
-    !attr ||
-    isNullTextHref(attr) ||
-    (attr.type === 'JSXExpressionContainer' && isNullTextHref(attr.expression))
-  )
-}
-const isNullTextHref = (attr) => attr.type === 'Literal' && (attr.value === '' || attr.value === '#')
-const findSpreadAttr = (a) => a.type === 'JSXSpreadAttribute'
-
-const findHrefAttribute = (a) => a.name?.name == 'href'
-const findToAttribute = (a) => a.name?.name == 'to'
-
-const MESSAGE_SUFFIX = ` に href 属性を正しく設定してください
+const MESSAGE_SUFFIX = ` に href${OPTION.react_router ? '、もしくはto' : ''} 属性を正しく設定してください
  - onClickなどでページ遷移する場合でもhref属性に遷移先のURIを設定してください
    - Cmd + clickなどのキーボードショートカットに対応出来ます
  - onClickなどの動作がURLの変更を行わない場合、button要素でマークアップすることを検討してください
@@ -115,19 +77,22 @@ module.exports = {
   },
   create(context) {
     const option = context.options[0] || {}
-    const checkType = option.checkType || 'always'
+    const spreadAttributeSelector = option.checkType === 'allow-spread-attributes' ? ':not(:has(JSXSpreadAttribute))' : ''
+    const reporter = (node) => {
+      context.report({
+        node,
+        message: `${node.name.name}${MESSAGE_SUFFIX}`,
+      })
+    }
 
     return {
-      JSXOpeningElement: (node) => {
-        const nodeName = check(node, checkType)
-
-        if (nodeName) {
-          context.report({
-            node,
-            message: `${nodeName}${MESSAGE_SUFFIX}`,
-          })
+      [`${ANCHOR_ELEMENT}:not(:has(${HREF_ATTRIBUTE}))${spreadAttributeSelector}`]: (node) => {
+        if (!OPTION.nextjs || !nextCheck(node)) {
+          reporter(node)
         }
       },
+      [`${ANCHOR_ELEMENT}:has(${HREF_ATTRIBUTE}[value=null])`]: reporter,
+      [`${ANCHOR_ELEMENT}:has(${HREF_ATTRIBUTE}:has(Literal[value=/^(|#)$/]))`]: reporter,
     }
   },
 }

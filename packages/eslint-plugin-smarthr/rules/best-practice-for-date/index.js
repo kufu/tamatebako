3340f4c0b4eb75ba1ba4caedb2a73f21f0336806
Mizoue Atsushi <asionfb@gmail.com>
2025-10-22 07:02:37 +0900

    chore: best-practice-for-dateの内部ロジックをselector形式に変更 (#859)

--- packages/eslint-plugin-smarthr/rules/best-practice-for-date/index.js
+++ packages/eslint-plugin-smarthr/rules/best-practice-for-date/index.js
@@ -1,10 +1,3 @@
-const MESSAGE_NEW_DATE = `'new Date(arg)' のように引数を一つだけ指定したDate instanceの生成は実行環境によって結果が異なるため、以下のいずれかの方法に変更してください
- - 'new Date(2022, 12 - 1, 31)' のように数値を個別に指定する
- - dayjsなど、日付系ライブラリを利用する (例:  'dayjs(arg).toDate()')`
-const MESSAGE_PARSE = `Date.parse は実行環境によって結果が異なるため、以下のいずれかの方法に変更してください
- - 'new Date(2022, 12 - 1, 31).getTime()' のように数値を個別に指定する
- - dayjsなど、日付系ライブラリを利用する (例: 'dayjs(arg).valueOf()')`
-
 const SEPARATOR = '(\/|-)'
 const DATE_REGEX = new RegExp(`^([0-9]{4})${SEPARATOR}([0-9]{1,2})${SEPARATOR}([0-9]{1,2})`)
 
@@ -36,29 +29,23 @@ module.exports = {
   },
   create(context) {
     return {
-      NewExpression: (node) => {
-        if (
-          node.callee.name === 'Date' &&
-          node.arguments.length == 1
-        ) {
-          context.report({
-            node,
-            message: MESSAGE_NEW_DATE,
-            fix: (fixer) => fixAction(fixer, node),
-          });
-        }
+      'NewExpression[callee.name="Date"][arguments.length=1]': (node) => {
+        context.report({
+          node,
+          message: `'new Date(arg)' のように引数を一つだけ指定したDate instanceの生成は実行環境によって結果が異なるため、以下のいずれかの方法に変更してください
+ - 'new Date(2022, 12 - 1, 31)' のように数値を個別に指定する
+ - dayjsなど、日付系ライブラリを利用する (例:  'dayjs(arg).toDate()')`,
+          fix: (fixer) => fixAction(fixer, node),
+        });
       },
-      CallExpression: (node) => {
-        if (
-          node.callee.object?.name === 'Date' &&
-          node.callee.property?.name === 'parse'
-        ) {
-          context.report({
-            node,
-            message: MESSAGE_PARSE,
-            fix: (fixer) => fixAction(fixer, node, '.getTime()'),
-          });
-        }
+      'CallExpression[callee.object.name="Date"][callee.property.name="parse"]': (node) => {
+        context.report({
+          node,
+          message: `Date.parse は実行環境によって結果が異なるため、以下のいずれかの方法に変更してください
+ - 'new Date(2022, 12 - 1, 31).getTime()' のように数値を個別に指定する
+ - dayjsなど、日付系ライブラリを利用する (例: 'dayjs(arg).valueOf()')`,
+          fix: (fixer) => fixAction(fixer, node, '.getTime()'),
+        });
       },
     }
   },

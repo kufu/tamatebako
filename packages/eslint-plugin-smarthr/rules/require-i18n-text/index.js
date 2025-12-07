5f8c5464113879280947273281ec7f16b0e7d3c8
AtsushiM <at.mizoue@gmail.com>
2025-12-08 08:59:44 +0900

    fix: require-i18n-textで特定の文字列の場合、修正対象にしないように調整

--- packages/eslint-plugin-smarthr/rules/require-i18n-text/index.js
+++ packages/eslint-plugin-smarthr/rules/require-i18n-text/index.js
@@ -35,6 +35,9 @@ const STRING_LITERAL_CONDITION =
 const generateAttributeSelector = (attributes) =>
   `JSXAttribute[name.name=/^(${attributes.join('|')})$/]${STRING_LITERAL_CONDITION}`
 
+const REGEX_IGNORE_TEXT = /^ *(\.|\+|\-|\*|\/|[0-9]+) *$/
+const checkIgnoreText = (text) => !REGEX_IGNORE_TEXT.test(text)
+
 /**
  * @type {import('@typescript-eslint/utils').TSESLint.RuleModule<''>}
  */
@@ -44,49 +47,52 @@ module.exports = {
     schema: SCHEMA,
   },
   create(context) {
-    const options = context.options[0] || {}
-    const elementsObj = options.elements || {}
-
+    const elementsObj = (context.options[0] || {}).elements || {}
     // ユーザーが'*'を設定していない場合のみデフォルトを適用
     const wildcardAttributes = elementsObj['*'] || DEFAULT_WILDCARD_ATTRIBUTES
     const specificElements = Object.keys(elementsObj).filter((k) => k !== '*')
     const handlers = {}
 
     const reportAttributeError = (node) => {
-      context.report({
-        node,
-        message: `${node.parent.name.name}の${node.name.name}属性に文字列リテラルが指定されています。多言語化対応のため、翻訳関数を使用してください
+      if (checkIgnoreText(node.value.value)) {
+        context.report({
+          node,
+          message: `${node.parent.name.name}の${node.name.name}属性に文字列リテラルが指定されています。多言語化対応のため、翻訳関数を使用してください
  - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/require-i18n-text`,
-      })
+        })
+      }
     }
 
     // 個別要素の設定
     for (const elementName of specificElements) {
       const attributes = elementsObj[elementName]
-      if (attributes.length === 0) continue
 
-      handlers[`JSXOpeningElement[name.name="${elementName}"] > ${generateAttributeSelector(attributes)}`] = reportAttributeError
+      if (attributes.length !== 0) {
+        handlers[`JSXOpeningElement[name.name="${elementName}"] > ${generateAttributeSelector(attributes)}`] = reportAttributeError
+      }
     }
 
     // ワイルドカード設定
     if (wildcardAttributes && wildcardAttributes.length > 0) {
       const attributeSelector = generateAttributeSelector(wildcardAttributes)
-      if (specificElements.length > 0) {
-        // 個別設定要素を除外
-        handlers[`JSXOpeningElement:not([name.name=/^(${specificElements.join('|')})$/]) > ${attributeSelector}`] =
-          reportAttributeError
-      } else {
-        handlers[attributeSelector] = reportAttributeError
-      }
+
+      handlers[
+        specificElements.length > 0
+          // 個別設定要素を除外
+          ? `JSXOpeningElement:not([name.name=/^(${specificElements.join('|')})$/]) > ${attributeSelector}`
+          : attributeSelector
+      ] = reportAttributeError
     }
 
     // 子要素の文字列リテラルチェック（空白のみのテキストは除外）
     handlers['JSXText[value=/\\S/]'] = (node) => {
-      context.report({
-        node,
-        message: `子要素に文字列リテラルが指定されています。多言語化対応のため、翻訳関数を使用してください
+      if (checkIgnoreText(node.value)) {
+        context.report({
+          node,
+          message: `子要素に文字列リテラルが指定されています。多言語化対応のため、翻訳関数を使用してください
  - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/require-i18n-text`,
-      })
+        })
+      }
     }
 
     return handlers

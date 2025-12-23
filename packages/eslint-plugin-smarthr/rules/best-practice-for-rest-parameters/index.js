fd4010041dd889ecb5f5d27f8b73a1e4bb4dcfdc
AtsushiM <at.mizoue@gmail.com>
2025-12-23 09:16:42 +0900

    fix: best-practice-for-rest-parametersの誤検知を修正する

--- packages/eslint-plugin-smarthr/rules/best-practice-for-rest-parameters/index.js
+++ packages/eslint-plugin-smarthr/rules/best-practice-for-rest-parameters/index.js
@@ -1,5 +1,9 @@
 const SCHEMA = []
 
+const MEMBER_EXPRESSION_REST_REGEX = /^rest\./
+const DETAIL_LINK = `
+ - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/best-practice-for-rest-parameters`
+
 /**
  * @type {import('@typescript-eslint/utils').TSESLint.RuleModule<''>}
  */
@@ -9,26 +13,36 @@ module.exports = {
     schema: SCHEMA,
   },
   create(context) {
-    const restAction = (node) => {
+    const actionNotRest = (node) => {
       context.report({
         node,
-        message: `残余引数以外に 'rest' という名称を利用しないでください
- - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/best-practice-for-rest-parameters
+        message: `残余引数以外に 'rest' という名称を利用しないでください${DETAIL_LINK}
  - 残余引数(rest parameters)と混同する可能性があるため別の名称に修正してください`,
       });
     }
+    const actionMemberExpressionName = (node) => {
+      if (node.parent.type === 'MemberExpression') {
+        return actionMemberExpressionName(node.parent)
+      }
 
-    return {
-      [`RestElement[argument.name='props']`]: (node) => {
+      if (MEMBER_EXPRESSION_REST_REGEX.test(context.sourceCode.getText(node))){
         context.report({
           node,
-          message: `残余引数には 'props' という名称を利用しないでください
- - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/best-practice-for-rest-parameters
- - 'rest' という名称を推奨します`,
+          message: `残余引数内の属性を参照しないでください${DETAIL_LINK}`,
+        });
+      }
+    }
+
+    return {
+      [`RestElement:not([argument.name='rest'])`]: (node) => {
+        context.report({
+          node,
+          message: `残余引数には 'rest' という名称を指定してください${DETAIL_LINK}`,
         });
       },
-      [`:not(:matches(RestElement,JSXSpreadAttribute,ObjectPattern>Property,ObjectExpression>Property))>Identifier[name='rest']`]: restAction,
-      [`:matches(ObjectPattern>Property[value.name='rest'],ObjectExpression>Property[key.name='rest'])`]: restAction,
+      [`:not(:matches(RestElement,JSXSpreadAttribute,JSXSpreadAttribute>TSAsExpression,SpreadElement,SpreadElement>TSAsExpression,MemberExpression,VariableDeclarator,ArrayExpression,CallExpression,ObjectPattern>Property,ObjectExpression>Property))>Identifier[name='rest']`]: actionNotRest,
+      [`:matches(VariableDeclarator[id.name='rest'],ObjectPattern>Property[value.name='rest'],ObjectExpression>Property[key.name='rest'])`]: actionNotRest,
+      [`MemberExpression[object.name='rest']`]: actionMemberExpressionName,
     }
   },
 }
--- packages/eslint-plugin-smarthr/test/best-practice-for-rest-parameters.js
+++ packages/eslint-plugin-smarthr/test/best-practice-for-rest-parameters.js
@@ -11,34 +11,39 @@ const ruleTester = new RuleTester({
   },
 })
 
-const ERROR_PROPS = `残余引数には 'props' という名称を利用しないでください
- - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/best-practice-for-rest-parameters
- - 'rest' という名称を推奨します`
-const ERROR_REST = `残余引数以外に 'rest' という名称を利用しないでください
- - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/best-practice-for-rest-parameters
+const DETAIL_LINK = `
+ - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/best-practice-for-rest-parameters`
+const ERROR_REST_NAME = `残余引数には 'rest' という名称を指定してください${DETAIL_LINK}`
+const ERROR_NOT_REST_NAME = `残余引数以外に 'rest' という名称を利用しないでください${DETAIL_LINK}
  - 残余引数(rest parameters)と混同する可能性があるため別の名称に修正してください`
+const ERROR_REST_CHILD_REF = `残余引数内の属性を参照しないでください${DETAIL_LINK}`
 
 ruleTester.run('best-practice-for-rest-parameters', rule, {
   valid: [
     { code: `const hoge = (a, b, ...rest) => {}` },
     { code: `const hoge = ({ a, b, ...rest }) => {}` },
-    { code: `const hoge = (a, b, ...hoge) => {}` },
-    { code: `const hoge = ({ a, b, ...xxxProps }) => {}` },
     { code: `const hoge = (props) => {}` },
     { code: `const props = {}` },
     { code: `const hogeRest = {}` },
     { code: `const hoge = { fuga: rest }` },
+    { code: `const hoge = rest` },
+    { code: `const hoge = hoge.rest.fuga` },
+    { code: `const hoge = { ...rest }` },
+    { code: `const hoge = [rest]` },
+    { code: `hoge(rest)` },
     { code: `<Any {...rest} />` },
   ],
   invalid: [
-    { code: `const hoge = (a, b, ...props) => {}`, errors: [ { message: ERROR_PROPS } ] },
-    { code: `const hoge = ({ a, b, ...props }) => {}`, errors: [ { message: ERROR_PROPS } ] },
-    { code: `const hoge = (rest) => {}`, errors: [ { message: ERROR_REST } ] },
-    { code: `const hoge = (a, b, rest) => {}`, errors: [ { message: ERROR_REST } ] },
-    { code: `const hoge = ({ a: rest, b }) => {}`, errors: [ { message: ERROR_REST } ] },
-    { code: `const rest = {}`, errors: [ { message: ERROR_REST } ] },
-    { code: `const hoge = { rest }`, errors: [ { message: ERROR_REST } ] },
-    { code: `const hoge = { rest: fuga }`, errors: [ { message: ERROR_REST } ] },
+    { code: `const hoge = (a, b, ...props) => {}`, errors: [ { message: ERROR_REST_NAME } ] },
+    { code: `const hoge = ({ a, b, ...props }) => {}`, errors: [ { message: ERROR_REST_NAME } ] },
+    { code: `const hoge = (rest) => {}`, errors: [ { message: ERROR_NOT_REST_NAME } ] },
+    { code: `const hoge = (a, b, rest) => {}`, errors: [ { message: ERROR_NOT_REST_NAME } ] },
+    { code: `const hoge = ({ a: rest, b }) => {}`, errors: [ { message: ERROR_NOT_REST_NAME } ] },
+    { code: `const rest = {}`, errors: [ { message: ERROR_NOT_REST_NAME } ] },
+    { code: `const hoge = { rest }`, errors: [ { message: ERROR_NOT_REST_NAME } ] },
+    { code: `const hoge = { rest: fuga }`, errors: [ { message: ERROR_NOT_REST_NAME } ] },
+    { code: `const hoge = rest.hoge`, errors: [ { message: ERROR_REST_CHILD_REF } ] },
+    { code: `const hoge = rest.hoge.fuga`, errors: [ { message: ERROR_REST_CHILD_REF } ] },
   ]
 })
 

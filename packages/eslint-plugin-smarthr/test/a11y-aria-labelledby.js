31d4649793c8befe9c7198c667f5e951b1b8b83c
Mizoue Atsushi <asionfb@gmail.com>
2026-03-04 17:12:46 +0900

    feat: a11y-aria-labelledby ruleを追加 (#1099)

---
+++ packages/eslint-plugin-smarthr/test/a11y-aria-labelledby.js
@@ -0,0 +1,30 @@
+const rule = require('../rules/a11y-aria-labelledby')
+const RuleTester = require('eslint').RuleTester
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+  },
+})
+
+const ERROR_MESSAGE = `aria-labelledby属性には画面上に存在する別要素に指定したid属性と同じ値を指定する必要があります。
+ - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/a11y-aria-labelledby
+ - 設定する値を変数化したうえでaria-labelledby属性に設定してください`
+
+ruleTester.run('a11y-aria-labelledby', rule, {
+  valid: [
+    { code: `<Any aria-labelledby={hoge} />` },
+    { code: `<Any aria-labelledby={obj.attr} />` },
+    { code: `<Any aria-labelledby={hoge.fuga.toString()} />` },
+    { code: '<Any aria-labelledby={`${hoge} ${fuga}`} />' },
+  ],
+  invalid: [
+    { code: `<Any aria-labelledby="hoge" />`, errors: [{ message: ERROR_MESSAGE }] },
+    { code: '<Any aria-labelledby={`hoge-${fuga}`} />', errors: [{ message: ERROR_MESSAGE }] },
+    { code: '<Any aria-labelledby={`${"hoge"} ${fuga}`} />', errors: [{ message: ERROR_MESSAGE }] },
+  ]
+})

a9ebca94bfec8efb35e1f52fc007a1521358efce
Mizoue Atsushi <asionfb@gmail.com>
2026-03-11 14:01:51 +0900

    feat(a11y-trigger-has-button): DisclosureTriggerに対応 (#1116)

--- packages/eslint-plugin-smarthr/test/a11y-trigger-has-button.js
+++ packages/eslint-plugin-smarthr/test/a11y-trigger-has-button.js
@@ -14,7 +14,7 @@ ruleTester.run('a11y-trigger-has-button', rule, {
   valid: [
     { code: '<DropdownTrigger><button>hoge</button></DropdownTrigger>' },
     { code: '<DialogTrigger><AnyButton>{hoge}</AnyButton></DialogTrigger>' },
-    { code: '<DropdownTrigger>{hoge}</DropdownTrigger>' },
+    { code: '<DisclosureTrigger>{hoge}</DisclosureTrigger>' },
     { code: '<AnyDropdownTrigger/>' },
   ],
   invalid: [
@@ -22,13 +22,13 @@ ruleTester.run('a11y-trigger-has-button', rule, {
  - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/a11y-trigger-has-button` } ] },
     { code: '<DialogTrigger><span><Button>ほげ</Button></span></DialogTrigger>', errors: [ { message: `DialogTrigger の直下にはbutton要素のみ設置してください(AnchorButtonはa要素のため設置できません)
  - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/a11y-trigger-has-button` } ] },
-    { code: '<DropdownTrigger><AnchorButton>ほげ</AnchorButton></DropdownTrigger>', errors: [ { message: `DropdownTrigger の直下にはbutton要素のみ設置してください(AnchorButtonはa要素のため設置できません)
+    { code: '<DisclosureTrigger><AnchorButton>ほげ</AnchorButton></DisclosureTrigger>', errors: [ { message: `DisclosureTrigger の直下にはbutton要素のみ設置してください(AnchorButtonはa要素のため設置できません)
  - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/a11y-trigger-has-button` } ] },
     { code: '<DropdownTrigger><ButtonAnchor>ほげ</ButtonAnchor></DropdownTrigger>', errors: [ { message: `DropdownTrigger の直下にはbutton要素のみ設置してください(AnchorButtonはa要素のため設置できません)
  - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/a11y-trigger-has-button` } ] },
     { code: '<DialogTrigger><button>{hoge}</button>{hoge}</DialogTrigger>', errors: [ { message: `DialogTrigger の直下には複数のコンポーネントを設置することは出来ません。button要素が一つだけ設置されている状態にしてください
  - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/a11y-trigger-has-button` } ] },
-    { code: '<DropdownTrigger>{hoge}<span>text</span></DropdownTrigger>', errors: [ { message: `DropdownTrigger の直下には複数のコンポーネントを設置することは出来ません。button要素が一つだけ設置されている状態にしてください
+    { code: '<DisclosureTrigger>{hoge}<span>text</span></DisclosureTrigger>', errors: [ { message: `DisclosureTrigger の直下には複数のコンポーネントを設置することは出来ません。button要素が一つだけ設置されている状態にしてください
  - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/a11y-trigger-has-button` } ] },
   ]
 })

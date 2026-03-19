2b3861e941c7496fad42e0335583f86bc105d44e
AtsushiM <at.mizoue@gmail.com>
2026-03-19 11:40:28 +0900

    Revert "refactor: セレクタを事前定数化してパフォーマンス改善"
    This reverts commit fd13c53269631be2cc884eab60c8e7f4ad50c5a4.

--- packages/eslint-plugin-smarthr/rules/trim-props/index.js
+++ packages/eslint-plugin-smarthr/rules/trim-props/index.js
@@ -1,6 +1,5 @@
 const SCHEMA = []
 
-const TRIM_SELECTOR = ':matches(JSXAttribute > Literal[value=/(^ | $)/], JSXAttribute > JSXExpressionContainer > :matches(Literal[value=/(^ | $)/], TemplateLiteral:has(> TemplateElement:matches(:first-child[value.raw=/^ /],:last-child[value.raw=/ $/]))))'
 
 /**
  * @type {import('@typescript-eslint/utils').TSESLint.RuleModule<''>}
@@ -13,7 +12,7 @@ module.exports = {
   },
   create(context) {
     return {
-      [TRIM_SELECTOR]: (node) => {
+      ':matches(JSXAttribute > Literal[value=/(^ | $)/], JSXAttribute > JSXExpressionContainer > :matches(Literal[value=/(^ | $)/], TemplateLiteral:has(> TemplateElement:matches(:first-child[value.raw=/^ /],:last-child[value.raw=/ $/]))))': (node) => {
         context.report({
           node,
           message: `属性に設定している文字列から先頭、末尾の空白文字を削除してください

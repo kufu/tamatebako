d2345a0623ebbf0f805c7c8a6b2b957ac21b0ffd
chigusa <83741212+chigusa1231@users.noreply.github.com>
2026-04-09 18:55:28 +0900

    fix: trim-propsのセレクターをesquery互換へ修正し、ESLint 9.xでのクラッシュを解消 (#1214)

--- packages/eslint-plugin-smarthr/rules/trim-props/index.js
+++ packages/eslint-plugin-smarthr/rules/trim-props/index.js
@@ -12,15 +12,23 @@ module.exports = {
     fixable: 'whitespace',
   },
   create(context) {
-    return {
-      ':matches(JSXAttribute > Literal[value=/(^ | $)/], JSXAttribute > JSXExpressionContainer > TemplateLiteral:has(> TemplateElement:matches(:first-child[value.raw=/^ /],:last-child[value.raw=/ $/])))': (node) => {
-        context.report({
-          node,
-          message: `属性に設定している文字列から先頭、末尾の空白文字を削除してください
+    const checker = (node) => {
+      context.report({
+        node,
+        message: `属性に設定している文字列から先頭、末尾の空白文字を削除してください
  - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/trim-props`,
-          fix: (fixer) => fixer.replaceText(node, context.sourceCode.getText(node).replace(TRIM_REGEX, '$1$2$3')),
-        })
-      },
+        fix: (fixer) => fixer.replaceText(node, context.sourceCode.getText(node).replace(TRIM_REGEX, '$1$2$3')),
+      })
+    }
+
+    return {
+      // esquery は :matches()・:has() 内での > (子結合子) を解釈できないため、
+      // 1つの複合セレクターではなく個別のセレクターに分割している
+      'JSXAttribute > Literal[value=/(^ | $)/]': checker,
+      'JSXAttribute > JSXExpressionContainer > TemplateLiteral > TemplateElement:matches(:first-child[value.raw=/^ /],:last-child[value.raw=/ $/])':
+        (node) => {
+          checker(node.parent)
+        },
     }
   },
 }

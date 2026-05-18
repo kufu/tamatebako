9bb4b5ae55ad8d1b9e4c7b9493138e1d4b8f2fe0
Mizoue Atsushi <asionfb@gmail.com>
2026-05-19 07:40:07 +0900

    refactor: best-practice-for-consecutive-definition-listをセレクター方式に書き換え (#1321)

--- packages/eslint-plugin-smarthr/rules/best-practice-for-consecutive-definition-list/index.js
+++ packages/eslint-plugin-smarthr/rules/best-practice-for-consecutive-definition-list/index.js
@@ -2,30 +2,22 @@
  * @type {import('@typescript-eslint/utils').TSESLint.RuleModule<''>}
  */
 
-const DEFINITION_LIST_PATTERN = /DefinitionList$/
+const DEFINITION_LIST = 'JSXElement[openingElement.name.name=/DefinitionList$/]'
+const WHITESPACE_TEXT = 'JSXText[value=/^\\s*$/]'
+const EMPTY_EXPRESSION = 'JSXExpressionContainer[expression.type="JSXEmptyExpression"]'
 
-const getPreviousSibling = (node) => {
-  const parent = node.parent
-  if (!parent || (parent.type !== 'JSXElement' && parent.type !== 'JSXFragment')) {
-    return null
-  }
-
-  const children = parent.children || []
-  const currentIndex = children.indexOf(node)
-
-  for (let i = currentIndex - 1; i >= 0; i--) {
-    const child = children[i]
-
-    if (
-      (child.type !== 'JSXText' || child.value.trim() !== '') &&
-      (child.type !== 'JSXExpressionContainer' || child.expression.type !== 'JSXEmptyExpression')
-    ) {
-      return child
-    }
-  }
-
-  return null
-}
+// 直接隣接（A + B）
+const ADJACENT_DIRECT = `${DEFINITION_LIST} + ${DEFINITION_LIST}`
+// 空白・改行のみのJSXText経由（A + W + B）
+const ADJACENT_WITH_WHITESPACE = `${DEFINITION_LIST} + ${WHITESPACE_TEXT} + ${DEFINITION_LIST}`
+// JSXコメント（{/* */}、{}）経由（A + E + B）
+const ADJACENT_WITH_COMMENT = `${DEFINITION_LIST} + ${EMPTY_EXPRESSION} + ${DEFINITION_LIST}`
+// 空白・改行 + JSXコメント（A + W + E + B）
+const ADJACENT_WHITESPACE_COMMENT = `${DEFINITION_LIST} + ${WHITESPACE_TEXT} + ${EMPTY_EXPRESSION} + ${DEFINITION_LIST}`
+// JSXコメント + 空白・改行（A + E + W + B）
+const ADJACENT_COMMENT_WHITESPACE = `${DEFINITION_LIST} + ${EMPTY_EXPRESSION} + ${WHITESPACE_TEXT} + ${DEFINITION_LIST}`
+// 空白・改行 + JSXコメント + 空白・改行（A + W + E + W + B）
+const ADJACENT_WHITESPACE_COMMENT_WHITESPACE = `${DEFINITION_LIST} + ${WHITESPACE_TEXT} + ${EMPTY_EXPRESSION} + ${WHITESPACE_TEXT} + ${DEFINITION_LIST}`
 
 module.exports = {
   meta: {
@@ -33,20 +25,23 @@ module.exports = {
     schema: [],
   },
   create(context) {
-    return {
-      [`JSXElement[openingElement.name.name=${DEFINITION_LIST_PATTERN}]`](node) {
-        const prev = getPreviousSibling(node)
-
-        if (prev?.type === 'JSXElement' && DEFINITION_LIST_PATTERN.test(prev.openingElement.name.name)) {
-          context.report({
-            node: node.openingElement.name,
-            message: `DefinitionList が連続しています
+    const reporter = (node) => {
+      context.report({
+        node,
+        message: `DefinitionList が連続しています
  - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/best-practice-for-consecutive-definition-list
  - DefinitionListItem の maxColumns prop を使用して1つにまとめることを検討してください
  - 例外: 意味的に異なるグループの場合は複数のDefinitionListを使用しても問題ありません`,
-          })
-        }
-      },
+      })
+    }
+
+    return {
+      [ADJACENT_DIRECT]: reporter,
+      [ADJACENT_WITH_WHITESPACE]: reporter,
+      [ADJACENT_WITH_COMMENT]: reporter,
+      [ADJACENT_WHITESPACE_COMMENT]: reporter,
+      [ADJACENT_COMMENT_WHITESPACE]: reporter,
+      [ADJACENT_WHITESPACE_COMMENT_WHITESPACE]: reporter,
     }
   },
 }

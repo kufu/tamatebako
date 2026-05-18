5a0a4291f2be64019baa1b3e1e44d0b015deeca8
AtsushiM <at.mizoue@gmail.com>
2026-05-19 06:57:38 +0900

    refactor: best-practice-for-consecutive-definition-listをセレクター方式に書き換え

--- packages/eslint-plugin-smarthr/rules/best-practice-for-consecutive-definition-list/index.js
+++ packages/eslint-plugin-smarthr/rules/best-practice-for-consecutive-definition-list/index.js
@@ -2,30 +2,13 @@
  * @type {import('@typescript-eslint/utils').TSESLint.RuleModule<''>}
  */
 
-const DEFINITION_LIST_PATTERN = /DefinitionList$/
-
-const getPreviousSibling = (node) => {
-  const parent = node.parent
-  if (!parent || (parent.type !== 'JSXElement' && parent.type !== 'JSXFragment')) {
-    return null
-  }
-
-  const children = parent.children || []
-  const currentIndex = children.indexOf(node)
-
-  for (let i = currentIndex - 1; i >= 0; i--) {
-    const child = children[i]
-
-    if (
-      (child.type !== 'JSXText' || child.value.trim() !== '') &&
-      (child.type !== 'JSXExpressionContainer' || child.expression.type !== 'JSXEmptyExpression')
-    ) {
-      return child
-    }
-  }
-
-  return null
-}
+const DEFINITION_LIST = 'JSXElement[openingElement.name.name=/DefinitionList$/]'
+const WHITESPACE_TEXT = 'JSXText[value=/^\\s*$/]'
+const EMPTY_EXPRESSION = 'JSXExpressionContainer[expression.type="JSXEmptyExpression"]'
+const ERROR_MESSAGE = `DefinitionList が連続しています
+ - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/best-practice-for-consecutive-definition-list
+ - DefinitionListItem の maxColumns prop を使用して1つにまとめることを検討してください
+ - 例外: 意味的に異なるグループの場合は複数のDefinitionListを使用しても問題ありません`
 
 module.exports = {
   meta: {
@@ -33,20 +16,26 @@ module.exports = {
     schema: [],
   },
   create(context) {
-    return {
-      [`JSXElement[openingElement.name.name=${DEFINITION_LIST_PATTERN}]`](node) {
-        const prev = getPreviousSibling(node)
+    const reporter = (node) => {
+      context.report({
+        node: node.openingElement.name,
+        message: ERROR_MESSAGE,
+      })
+    }
 
-        if (prev?.type === 'JSXElement' && DEFINITION_LIST_PATTERN.test(prev.openingElement.name.name)) {
-          context.report({
-            node: node.openingElement.name,
-            message: `DefinitionList が連続しています
- - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/best-practice-for-consecutive-definition-list
- - DefinitionListItem の maxColumns prop を使用して1つにまとめることを検討してください
- - 例外: 意味的に異なるグループの場合は複数のDefinitionListを使用しても問題ありません`,
-          })
-        }
-      },
+    return {
+      // 1. 直接隣接
+      [`${DEFINITION_LIST} + ${DEFINITION_LIST}`]: reporter,
+      // 2. 空白のみJSXText経由
+      [`${DEFINITION_LIST} + ${WHITESPACE_TEXT} + ${DEFINITION_LIST}`]: reporter,
+      // 3. 空式/コメント経由
+      [`${DEFINITION_LIST} + ${EMPTY_EXPRESSION} + ${DEFINITION_LIST}`]: reporter,
+      // 4. 空白 + 空式/コメント
+      [`${DEFINITION_LIST} + ${WHITESPACE_TEXT} + ${EMPTY_EXPRESSION} + ${DEFINITION_LIST}`]: reporter,
+      // 5. 空式/コメント + 空白
+      [`${DEFINITION_LIST} + ${EMPTY_EXPRESSION} + ${WHITESPACE_TEXT} + ${DEFINITION_LIST}`]: reporter,
+      // 6. 空白 + 空式/コメント + 空白
+      [`${DEFINITION_LIST} + ${WHITESPACE_TEXT} + ${EMPTY_EXPRESSION} + ${WHITESPACE_TEXT} + ${DEFINITION_LIST}`]: reporter,
     }
   },
 }

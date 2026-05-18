867d097a2eb1cf1ccb93fdaed9f9e7187c905eef
AtsushiM <at.mizoue@gmail.com>
2026-05-19 07:17:29 +0900

    refactor: セレクター定数名を意味のある名前に変更、エラーメッセージをreporter内に移動

--- packages/eslint-plugin-smarthr/rules/best-practice-for-consecutive-definition-list/index.js
+++ packages/eslint-plugin-smarthr/rules/best-practice-for-consecutive-definition-list/index.js
@@ -6,18 +6,18 @@ const DEFINITION_LIST = 'JSXElement[openingElement.name.name=/DefinitionList$/]'
 const WHITESPACE_TEXT = 'JSXText[value=/^\\s*$/]'
 const EMPTY_EXPRESSION = 'JSXExpressionContainer[expression.type="JSXEmptyExpression"]'
 
-// セレクターを事前定義（パフォーマンス最適化）
-const SELECTOR_1 = `${DEFINITION_LIST} + ${DEFINITION_LIST}`
-const SELECTOR_2 = `${DEFINITION_LIST} + ${WHITESPACE_TEXT} + ${DEFINITION_LIST}`
-const SELECTOR_3 = `${DEFINITION_LIST} + ${EMPTY_EXPRESSION} + ${DEFINITION_LIST}`
-const SELECTOR_4 = `${DEFINITION_LIST} + ${WHITESPACE_TEXT} + ${EMPTY_EXPRESSION} + ${DEFINITION_LIST}`
-const SELECTOR_5 = `${DEFINITION_LIST} + ${EMPTY_EXPRESSION} + ${WHITESPACE_TEXT} + ${DEFINITION_LIST}`
-const SELECTOR_6 = `${DEFINITION_LIST} + ${WHITESPACE_TEXT} + ${EMPTY_EXPRESSION} + ${WHITESPACE_TEXT} + ${DEFINITION_LIST}`
-
-const ERROR_MESSAGE = `DefinitionList が連続しています
- - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/best-practice-for-consecutive-definition-list
- - DefinitionListItem の maxColumns prop を使用して1つにまとめることを検討してください
- - 例外: 意味的に異なるグループの場合は複数のDefinitionListを使用しても問題ありません`
+// 直接隣接（A + B）
+const ADJACENT_DIRECT = `${DEFINITION_LIST} + ${DEFINITION_LIST}`
+// 空白・改行のみのJSXText経由（A + W + B）
+const ADJACENT_WITH_WHITESPACE = `${DEFINITION_LIST} + ${WHITESPACE_TEXT} + ${DEFINITION_LIST}`
+// JSXコメント（{/* */}、{}）経由（A + E + B）
+const ADJACENT_WITH_COMMENT = `${DEFINITION_LIST} + ${EMPTY_EXPRESSION} + ${DEFINITION_LIST}`
+// 空白・改行 + JSXコメント（A + W + E + B）
+const ADJACENT_WHITESPACE_COMMENT = `${DEFINITION_LIST} + ${WHITESPACE_TEXT} + ${EMPTY_EXPRESSION} + ${DEFINITION_LIST}`
+// JSXコメント + 空白・改行（A + E + W + B）
+const ADJACENT_COMMENT_WHITESPACE = `${DEFINITION_LIST} + ${EMPTY_EXPRESSION} + ${WHITESPACE_TEXT} + ${DEFINITION_LIST}`
+// 空白・改行 + JSXコメント + 空白・改行（A + W + E + W + B）
+const ADJACENT_WHITESPACE_COMMENT_WHITESPACE = `${DEFINITION_LIST} + ${WHITESPACE_TEXT} + ${EMPTY_EXPRESSION} + ${WHITESPACE_TEXT} + ${DEFINITION_LIST}`
 
 module.exports = {
   meta: {
@@ -27,24 +27,21 @@ module.exports = {
   create(context) {
     const reporter = (node) => {
       context.report({
-        node: node.openingElement.name,
-        message: ERROR_MESSAGE,
+        node,
+        message: `DefinitionList が連続しています
+ - 詳細: https://github.com/kufu/tamatebako/tree/master/packages/eslint-plugin-smarthr/rules/best-practice-for-consecutive-definition-list
+ - DefinitionListItem の maxColumns prop を使用して1つにまとめることを検討してください
+ - 例外: 意味的に異なるグループの場合は複数のDefinitionListを使用しても問題ありません`,
       })
     }
 
     return {
-      // 1. 直接隣接
-      [SELECTOR_1]: reporter,
-      // 2. 空白・改行のみのJSXText経由
-      [SELECTOR_2]: reporter,
-      // 3. JSXコメント（{/* */}、{}）経由
-      [SELECTOR_3]: reporter,
-      // 4. 空白・改行 + JSXコメント
-      [SELECTOR_4]: reporter,
-      // 5. JSXコメント + 空白・改行
-      [SELECTOR_5]: reporter,
-      // 6. 空白・改行 + JSXコメント + 空白・改行
-      [SELECTOR_6]: reporter,
+      [ADJACENT_DIRECT]: reporter,
+      [ADJACENT_WITH_WHITESPACE]: reporter,
+      [ADJACENT_WITH_COMMENT]: reporter,
+      [ADJACENT_WHITESPACE_COMMENT]: reporter,
+      [ADJACENT_COMMENT_WHITESPACE]: reporter,
+      [ADJACENT_WHITESPACE_COMMENT_WHITESPACE]: reporter,
     }
   },
 }
